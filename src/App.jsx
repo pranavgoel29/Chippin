be457ce997e0992063b64a98fd87040b7ba7fc7e
pranavgoel29 <pranavgoel29@gmail.com>
2023-04-19 19:40:05 +0530

    Connected backend to frontend and added graphql queries with codegen for queries and urql

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,28 @@ import { Pages } from "./routes";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import { Client, cacheExchange, fetchExchange, Provider } from "urql";
+
+const client = new Client({
+  url: "http://localhost:4000/graphql",
+  exchanges: [cacheExchange, fetchExchange],
+});
 
 const router = createBrowserRouter([
   { path: Pages.HOME, element: <Home /> },
   { path: Pages.LOGIN, element: <Login /> },
-  { path: Pages.REGISTER, element: <Register /> }
+  { path: Pages.REGISTER, element: <Register /> },
 ]);
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
+    <Provider value={client}>
+      <div className="App">
+        <GlobalStyle />
 
-      <RouterProvider router={router} />
-    </div>
+        <RouterProvider router={router} />
+      </div>
+    </Provider>
   );
 }
 
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -9,6 +9,8 @@ import {
 } from "../utils/types";
 import { regex } from "../utils";
 import styled from "styled-components";
+import { useRegisterMutation } from "../generated/graphql";
+import { useNavigate } from "react-router-dom";
 
 const FormWrapper = styled.div`
   height: 100vh;
@@ -16,7 +18,6 @@ const FormWrapper = styled.div`
   width: 100%;
   max-width: 800px;
   margin: auto;
-
   .container {
     padding: 40px;
     h2 {
@@ -29,6 +30,7 @@ const FormWrapper = styled.div`
 `;
 
 const Register: React.FC<EditProfileInputs> = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -36,14 +38,26 @@ const Register: React.FC<EditProfileInputs> = () => {
     control,
   } = useForm<EditProfileInputs>({});
 
+  // We will be using these custom hooks instead of using useMutation-hook from 'urql'.
+  const [user, registerUser] = useRegisterMutation();
   const onSubmit: SubmitHandler<any> = async (fulldata) => {
-    console.log(fulldata);
+    console.log("data in form: ", fulldata);
+    const response = await registerUser(fulldata);
+    console.log("id: ", response?.data?.register?.user?.id);
+
+    // Handling errors;
+    if (response.data?.register.errors) {
+      console.log(response.data.register.errors);
+    } else if (response.data?.register.user) {
+      // Worked
+      navigate("/");
+    }
   };
 
   return (
     <FormWrapper>
       <div className="container">
-        <h2>Hello</h2>
+        <h2>Register</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
           <InputField
             label="User Name"
@@ -71,7 +85,7 @@ const Register: React.FC<EditProfileInputs> = () => {
             variant="contained"
             style={{ backgroundColor: "#202020" }}
           >
-            Contained
+            Register
           </Button>
         </form>
       </div>
