b162df6a9d254164e14fd374615000f1db9a5652
pranavgoel29 <pranavgoel29@gmail.com>
2023-04-19 11:24:14 +0530

    Adding submit button and a wrapper for register-form component

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { SubmitHandler, UseFormRegister, useForm } from "react-hook-form";
 import InputField from "../common/Inputs";
-import { EditProfileInputs, FormRegisterInputs, HTMLInputTypes } from "../utils/types";
+import {
+  EditProfileInputs,
+  FormRegisterInputs,
+  HTMLInputTypes,
+} from "../utils/types";
 import { regex } from "../utils";
+import styled from "styled-components";
 
-// interface registerProps {}
+const FormWrapper = styled.div`
+  height: 100vh;
+  padding: 40px;
+  form {
+    width: 20em;
+  }
+`;
 
 const Register: React.FC<EditProfileInputs> = () => {
   const {
@@ -19,18 +30,22 @@ const Register: React.FC<EditProfileInputs> = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <InputField
-        type={HTMLInputTypes.TEXT}
-        register={register as UseFormRegister<FormRegisterInputs>}
-        errors={errors}
-        errorMessage="Invalid Full Name"
-        fieldToRegister={`fullName`}
-        placeHolder="Full Name"
-        required={true}
-        pattern={regex.fullName}
-      />
-    </form>
+    <FormWrapper>
+      <h2>Hello</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputField
+          type={HTMLInputTypes.TEXT}
+          register={register as UseFormRegister<FormRegisterInputs>}
+          errors={errors}
+          errorMessage="Invalid Full Name"
+          fieldToRegister={`fullName`}
+          placeHolder="Full Name"
+          required={true}
+          pattern={regex.fullName}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </FormWrapper>
   );
 };
 
