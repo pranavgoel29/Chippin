b884511a53daf6eb08d0af30f1866e319503b558
pranavgoel29 <pranavgoel29@gmail.com>
2023-04-25 01:49:51 +0530

    Handling logout cache

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,80 @@ import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 // import Dashboard from "./Pages/Home/Dashboard";
 
-import { Client, cacheExchange, fetchExchange, Provider } from "urql";
+import { Client, fetchExchange, Provider } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import {
+  LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
+  Query,
+  RegisterMutation,
+} from "./generated/graphql";
+import { UseMediaQueryOptions } from "@mui/material";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = new Client({
   url: "http://localhost:4000/graphql",
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [
+    cacheExchange({
+      updates: {
+        Mutation: {
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              () => ({ me: null })
+            );
+          },
+
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
   fetchOptions: {
     credentials: "include",
 
