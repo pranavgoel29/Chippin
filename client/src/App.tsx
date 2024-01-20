5a4fbf8e06ce3e4d249450751b913f6f1bf62adf
pranavgoel29 <pranavgoel29@gmail.com>
2024-01-21 01:36:46 +0530

    Updated graphql link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function betterUpdateQuery<Result, Query>(
 }
 
 const client = new Client({
-  url: "http://localhost:4000/graphql",
+  url: "https://chippin.onrender.com/graphql",
   exchanges: [
     cacheExchange({
       updates: {
